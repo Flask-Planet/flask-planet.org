38b1dddbec797f26e6de856f4effe2197f756577
David Carmichael <carmichaelits@gmail.com>
2023-02-11 14:56:15 +0000

    Added updated init

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 import os
 import secrets
 
+from dotenv import load_dotenv
 from flask import Flask
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from app.extensions import bigapp, db, logger
 
 os.environ["CONFIG_SECRET_KEY"] = secrets.token_urlsafe(128)
+
+load_dotenv()
 
 
 def create_app():
@@ -15,10 +18,13 @@ def create_app():
     bigapp.init_app(app)
     logger.init_app(app)
 
-    bigapp.import_models(from_folder="models")
-    db.init_app(app)
+    if os.environ.get("FLASK_PLANET_DEV", None) is not None:
+        bigapp.import_models(from_folder="models")
+        db.init_app(app)
+        bigapp.import_blueprints("blueprints")
+    else:
+        bigapp.import_blueprint("coming_soon")
 
-    bigapp.import_blueprints("blueprints")
     bigapp.import_theme("theme")
 
     @app.before_request
